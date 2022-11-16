eca079e03eb9f849e69634f508f84bc4193cd547
Xin Lin <lin.xin@ncs.com.sg>
2022-11-16 16:54:50 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -35,7 +35,6 @@ public class MyStringStackTest extends TestCase {
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
 		assertTrue (stack.isEmpty());
-		stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
 	}
